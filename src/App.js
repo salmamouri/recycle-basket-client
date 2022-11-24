8a6361905bc24a67321fcc564b557c3b786f0484
Salma Tabassum Mouri <mouriclub@gmail.com>
2022-11-24 20:08:47 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className="">
-     
+     <RouterProvider router ={router}></RouterProvider>
     </div>
   );
 }
